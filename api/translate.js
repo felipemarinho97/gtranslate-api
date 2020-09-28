a9708a2efda6fd0c44b44d3643220afa5b6fddb5
Felipe Marinho <felipevm97@gmail.com>
2020-09-28 09:31:17 -0300

    add home page and fix API

--- api/translate.js
+++ api/translate.js
@@ -1,8 +1,13 @@
 const { allowCors } = require("../util");
-const googleTranslateApi = require("google-translate-open-api").default;
+const {
+  parseMultiple,
+  default: googleTranslateApi,
+} = require("google-translate-open-api");
 
-export default allowCors((req, res) => {
+export default allowCors(async (req, res) => {
   const { query } = req;
+  const result = (await googleTranslateApi(query.text, query)).data[0];
+  const response = parseMultiple(result);
 
-  res.json(googleTranslateApi(query.text, query));
+  res.json(response);
 });
