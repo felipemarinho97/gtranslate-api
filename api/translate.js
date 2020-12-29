478c54c6c4875ac049a676080546c06713fd496a
Felipe Marinho <felipevm97@gmail.com>
2020-12-29 14:16:39 -0300

    add workaround

--- api/translate.js
+++ api/translate.js
@@ -4,10 +4,26 @@ const {
   default: googleTranslateApi,
 } = require("google-translate-open-api");
 
+const fetch = require('node-fetch');
+
 export default allowCors(async (req, res) => {
   const { query } = req;
-  const result = (await googleTranslateApi(query.text, query)).data[0];
-  const response = parseMultiple(result);
 
-  res.json(response);
+
+  try {
+
+    const result = (await googleTranslateApi(query.text, query)).data[0];
+    const response = parseMultiple(result);
+    
+    res.json(response);
+  } catch(e) // workaround 
+  {
+    const text = query.text.join('')
+    const result = (await fetch(`https://api-translate.azharimm.tk/translate?engine=google&text=${
+      encodeURI(text)}&to=${query.to}`).then(r => r.json()))
+    
+    if (result.status == true) {
+      res.json(result.data.targets)
+    }
+  }
 });
