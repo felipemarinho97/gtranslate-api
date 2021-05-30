6e504176115a946112b9fc68ac543a25b6222bb2
Felipe Marinho <felipevm97@gmail.com>
2021-05-30 11:58:02 -0300

    fix: remove workaround

--- api/translate.js
+++ api/translate.js
@@ -4,26 +4,16 @@ const {
   default: googleTranslateApi,
 } = require("google-translate-open-api");
 
-const fetch = require('node-fetch');
-
 export default allowCors(async (req, res) => {
   const { query } = req;
 
+  const result = (
+    await googleTranslateApi(query.text, {
+      ...query,
+      client: "dict-chrome-ex",
+    })
+  ).data[0];
+  const response = parseMultiple(result);
 
-  try {
-
-    const result = (await googleTranslateApi(query.text, query)).data[0];
-    const response = parseMultiple(result);
-    
-    res.json(response);
-  } catch(e) // workaround 
-  {
-    const text = query.text.join('')
-    const result = (await fetch(`https://api-translate.azharimm.tk/translate?engine=google&text=${
-      encodeURI(text)}&to=${query.to}`).then(r => r.json()))
-    
-    if (result.status == true) {
-      res.json(result.data.targets)
-    }
-  }
+  res.json(response);
 });
